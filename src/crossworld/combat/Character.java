5423acf59c36852c4b113f92495a81aac8cc4f11
davidmizra17 <david.mizrahi@correo.unimet.edu.ve>
2024-03-02 20:10:43 -0400

    🎨 Character methods implementation
    fundamental methods for the Character class

--- src/crossworld/combat/Character.java
+++ src/crossworld/combat/Character.java
@@ -4,6 +4,8 @@
  */
 package crossworld.combat;
 
+import java.util.Random;
+
 /**
  *
  * @author davidmizrahi
@@ -15,9 +17,11 @@ public class Character extends Thread{
     private int counter;
     
     public Character(int ID, int priorityLevel, int counter){
+        
         this.ID = ID;
         this.priorityLevel = priorityLevel;
         this.counter = counter;
+        
     }
 
     public int getID() {
@@ -42,6 +46,63 @@ public class Character extends Thread{
 
     public void setCounter(int counter) {
         this.counter = counter;
+    }
+    
+    public int AssignPriorityLevel(Character character){
+        
+        //variable to keep track of how many variables are of "quality"
+        int elementCounter = 0;
+        
+        boolean habilidades = false;
+        boolean puntosDeVida = false;
+        boolean fuerza = false;
+        boolean agilidad = false;
+        
+        
+        
+        Random random = new Random();
+        
+
+        
+        double ProbHabilidades = random.nextDouble();
+        double ProbPuntosDeVida = random.nextDouble();
+        double ProbFuerza = random.nextDouble();
+        double ProbAgilidad = random.nextDouble();
+        
+        
+        if(ProbHabilidades <= 0.6) elementCounter+= 1; habilidades = true;
+        if(ProbPuntosDeVida <= 0.7) elementCounter+= 1; puntosDeVida = true;
+        if(ProbFuerza <= 0.5) elementCounter+= 1; fuerza = true;
+        if(ProbAgilidad <= 0.4) elementCounter+= 1; agilidad = true;
+        
+        if(elementCounter >= 3) priorityLevel = 1;
+        else if(elementCounter == 2) priorityLevel = 2;
+        else if(elementCounter == 1) priorityLevel = 3;
+        
+        
+        
+        return priorityLevel;
+        
+    }
+    
+    public void EnqueueProcess(Character character){
+        
+        Queue q1 = new Queue();
+        Queue q2 = new Queue();
+        Queue q3 = new Queue();
+        
+        
+        
+        
+        int characterLevel = AssignPriorityLevel(character);
+        
+        if(characterLevel == 1) q1.enqueue(character);
+        else if(characterLevel == 2) q2.enqueue(character);
+        else if(characterLevel == 3) q3.enqueue(character);
+        
+        
+        
+        
     }
     
     
--- src/crossworld/combat/CrossWorldCombat.java
+++ src/crossworld/combat/CrossWorldCombat.java
@@ -4,6 +4,8 @@
  */
 package crossworld.combat;
 
+import java.util.Random;
+
 /**
  *
  * @author davidmizrahi
@@ -15,6 +17,19 @@ public class CrossWorldCombat {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Random random = new Random();
+        
+        double r = 0.0;
+        
+        for (int i = 0; i < 10; i++) {
+            r = random.nextDouble();
+            if(r <= 1) System.out.println("Doing something 40% of the time");
+            
+            else System.out.println("The rest 60%");
+            
+            
+            
+        }
     }
     
 }
